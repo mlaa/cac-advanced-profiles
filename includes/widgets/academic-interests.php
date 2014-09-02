1ab4b2d75de9b390343662da713c383e3fb23c92
Jonathan Reeve <jon.reeve@gmail.com>
2014-09-02 14:40:01 -0400

    make comma-separated interests into links, fixes #4

--- includes/widgets/academic-interests.php
+++ includes/widgets/academic-interests.php
@@ -9,4 +9,8 @@ class CACAP_Widget_Academic_Interests extends CACAP_Widget {
 		) );
 	}
 
+	// linkify comma-separated interests
+	public function display_content_markup( $value ) {
+		return xprofile_filter_link_profile_data( $value ); 
+	} 
 }
