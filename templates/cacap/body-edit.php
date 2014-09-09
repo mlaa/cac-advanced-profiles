d6d3b64a5ae067b7252f64fc17a5956d038de53e
Jonathan Reeve <jon.reeve@gmail.com>
2014-09-09 10:07:58 -0400

    attempt at visibility controls

--- templates/cacap/body-edit.php
+++ templates/cacap/body-edit.php
@@ -66,6 +66,32 @@
 				<input type="hidden" value="<?php echo esc_attr( $widget_instance->widget_type->slug ) ?>" name="<?php echo esc_attr( $widget_instance->css_id ) ?>[widget_type]" />
 
 				<a href="#" class="cacap-widget-remove button confirm"><?php _e( 'Remove', 'cacap' ) ?></a>
+
+				<!-- Visibility Stuff --> 
+
+					<?php do_action( 'bp_custom_profile_edit_fields_pre_visibility' ); ?>
+
+					<?php if ( bp_current_user_can( 'bp_xprofile_change_field_visibility' ) ) : ?>
+						<p class="field-visibility-settings-toggle" id="field-visibility-settings-toggle-<?php bp_the_profile_field_id() ?>">
+							<?php printf( __( 'This field can be seen by: <span class="current-visibility-level">%s</span>', 'buddypress' ), bp_get_the_profile_field_visibility_level_label() ) ?> <a href="#" class="visibility-toggle-link"><?php _e( 'Change', 'buddypress' ); ?></a>
+						</p>
+
+						<div class="field-visibility-settings" id="field-visibility-settings-<?php bp_the_profile_field_id() ?>">
+							<fieldset>
+								<legend><?php _e( 'Who can see this field?', 'buddypress' ) ?></legend>
+
+								<?php bp_profile_visibility_radio_buttons() ?>
+
+							</fieldset>
+							<a class="field-visibility-settings-close" href="#"><?php _e( 'Close', 'buddypress' ) ?></a>
+						</div>
+					<?php else : ?>
+						<div class="field-visibility-settings-notoggle" id="field-visibility-settings-toggle-<?php bp_the_profile_field_id() ?>">
+							<?php printf( __( 'This field can be seen by: <span class="current-visibility-level">%s</span>', 'buddypress' ), bp_get_the_profile_field_visibility_level_label() ) ?>
+						</div>
+					<?php endif ?>
+
+				<!-- End Visibility Stuff --> 
 			</li>
 		<?php endforeach; ?>
 		</ul>
