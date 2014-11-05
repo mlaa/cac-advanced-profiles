d8d9d887ef3b4a88504dc08e0a148dd7b446eb6c
Jonathan Reeve <jon.reeve@gmail.com>
2014-11-05 15:42:37 -0500

    remove MLA-customized interests linkifier

--- includes/widgets/academic-interests.php
+++ includes/widgets/academic-interests.php
@@ -12,6 +12,6 @@ class CACAP_Widget_Academic_Interests extends CACAP_Widget {
 
 	// linkify comma-separated interests
 	public function display_content_markup( $value ) {
-		return mla_xprofile_filter_link_profile_data( $value ); 
+		return xprofile_filter_link_profile_data( $value ); 
 	} 
 }
