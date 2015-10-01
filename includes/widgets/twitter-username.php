85094ecab01493619107c5ad78af1f58cf55bc47
chriszarate <chris@zarate.org>
2015-10-01 13:11:23 -0400

    Add required argument for wp_kses.

--- includes/widgets/twitter-username.php
+++ includes/widgets/twitter-username.php
@@ -6,16 +6,16 @@ class CACAP_Widget_Twitter_Username extends CACAP_Widget {
 			'name' => __( '<em>Twitter</em> User Name', 'cacap' ),
 			'slug' => 'twitter-username',
 			'content_type' => 'twitter_username',
-			'placeholder' => 'Your Twitter user name, without the @ symbol. Ours is &quot;MLACommons.&quot;', 
+			'placeholder' => 'Your Twitter user name, without the @ symbol. Ours is &quot;MLACommons.&quot;',
 		) );
 	}
 
 	// Italicize Twitter.
 	public function display_title_markup( $value ) {
-		return wp_kses( $this->name );
+		return wp_kses( $this->name, array() );
 	}
 
 	public function display_content_markup( $value ) {
-		return '<a href="http://www.twitter.com/' . $value . '">' . $value . '</a>'; 
-	} 
+		return '<a href="http://www.twitter.com/' . $value . '">' . $value . '</a>';
+	}
 }
